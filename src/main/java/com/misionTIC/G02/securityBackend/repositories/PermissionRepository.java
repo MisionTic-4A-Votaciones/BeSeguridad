2807929feaccd66ec5a034b664ba7a18dd7af953
Vanessa Nájera <vjndovale@gmail.com>
2022-11-10 09:28:00 -0500

    add missing line to repositories

--- src/main/java/com/misionTIC/G02/securityBackend/repositories/PermissionRepository.java
+++ src/main/java/com/misionTIC/G02/securityBackend/repositories/PermissionRepository.java
@@ -2,6 +2,8 @@ package com.misionTIC.G02.securityBackend.repositories;
 
 import com.misionTIC.G02.securityBackend.models.Permission;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PermissionRepository extends CrudRepository<Permission, Integer> {
 }
--- src/main/java/com/misionTIC/G02/securityBackend/repositories/RolRepository.java
+++ src/main/java/com/misionTIC/G02/securityBackend/repositories/RolRepository.java
@@ -2,6 +2,8 @@ package com.misionTIC.G02.securityBackend.repositories;
 
 import com.misionTIC.G02.securityBackend.models.Rol;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RolRepository extends CrudRepository<Rol,Integer> {
 }
